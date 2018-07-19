5950fb81cb26e80bde6a18a8282549582f2d7096
Elisa Cutrin <ecutrin@thoughtworks.com>
2018-07-19 16:31:34 +0200

    [+Jordi] Increased SUBMIT_T_TIMEOUT to 1 minute because it was timing out when using Chainspace on the boxes

--- contrib/core-tools/node-dist-template/start_client_api.sh
+++ contrib/core-tools/node-dist-template/start_client_api.sh
@@ -8,6 +8,6 @@ CLIENT_API_PORT=5000
 CLIENT_API_DB=../node_0_0/database
 MAIN_CLASS=uk.ac.ucl.cs.sec.chainspace.Client
 CONFIG_FILE=config/client-api/config.txt
-SUBMIT_T_TIMEOUT=20000
+SUBMIT_T_TIMEOUT=60000
 
 java -Dmapclient.submitTxTimeout=${SUBMIT_T_TIMEOUT} -Dclient.api.database=${CLIENT_API_DB} -Dclient.api.port=${CLIENT_API_PORT} -cp ${BFT_SMART_LIB}:${CHAINSPACE_JAR} ${MAIN_CLASS} ${CONFIG_FILE}
